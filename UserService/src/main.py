b9643ffd5daede8ac329caeedac254ee7a85fd89
assibering <asbjorn.bering@gmail.com>
2025-04-20 12:15:09 +1000

    not-so-hot reloading with compose --watch and rebuild

--- UserService/src/main.py
+++ UserService/src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .src.routes import UserController
+from .routes import UserController
 
 app = FastAPI()
 app.include_router(UserController.router)
