c246efc026530c29dc7b135092ab313322f106c8
assibering <asbjorn.bering@gmail.com>
2025-04-23 19:48:34 +1000

    database for testing FastAPI services without the automatic crypto fetch

---
+++ PostgresDB/setup_no_crypto.sql
@@ -0,0 +1,12 @@
+-- File: /opt/sql/setup.sql
+
+\connect crypto_db;
+
+CREATE EXTENSION IF NOT EXISTS http;
+CREATE EXTENSION IF NOT EXISTS pg_cron;
+
+-- 1. Setup schemas, tables
+\i /opt/sql/init_tables.sql
+
+-- 2. Setup permissions
+\i /opt/sql/authorised_services.sql
